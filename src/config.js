300b3ce3f109346b6bea9c5c62436285f2663a37
Kathleen Turner <kathleenturner@Kathleens-MBP.hsd1.co.comcast.net>
2019-07-18 15:32:24 -0600

    initial commit - react router and linking container working with not found page, still need to write route logic, config to aws set

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-tutorial-1"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://osuqsxs6i9.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_KCVLOl7sB",
+      APP_CLIENT_ID: "14riebskli29f02rph631515hr",
+      IDENTITY_POOL_ID: "us-east-2:89794458-2986-4f80-b038-0dfc49066ec9"
+    }
+  };
+  
